f2b3330c8814b3de61a2d4e0c371683616c99c30
Fedoseev Aleksandr <fedoseev@selectel.ru>
2020-11-26 13:29:02 +0300

    Fix tests and improve README.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # git_hooks
 Some git hooks
+
+### prepare_commit_msg.py
+git hook для добавления идентификатора задачи перед редактированием сообщения к коммиту. Устанавливать нужно на место **prepare-commit-msg**.
+
+```
+$ cd repo_name
+$ mv hook.py .git/hooks/prepare-commit-msg
+```
+
+Для того чтобы идентификатор задачи подставился в шаблон сообщения коммита нужно называть ветку по шаблону:
+{тип задачи}_{номер задачи}{описание}
+Примеры:
+| Имя ветки | Добавленный идентификатор |
+| ------ | ------ |
+| openstack_1_add_new_feature | OPENSTACK-1 |
+| bug_42069_fix_critical_bug | BUG-42069 |
+| feature_awesome | - |
+| quick_fix_3290 | QUICK-FIX-3290 |
--- tests/test_prepare_commit_msg.py
+++ tests/test_prepare_commit_msg.py
@@ -5,7 +5,8 @@ from hooks.prepare_commit_msg import create_commit_message
 
 
 def test_create_commit_message():
-    assert "Bug #11:\n" == create_commit_message("bug_11")
-    assert "Feature #2:\n" == create_commit_message("feature_2")
-    assert "Some branch name #1234:\n" == create_commit_message("some_branch_name_1234")
-    assert "" == create_commit_message("temporary_branch")
+    assert "\nBUG-11\n" == create_commit_message("bug_11")
+    assert "\nFEATURE-2\n" == create_commit_message("feature_2")
+    assert "\nSOME-BRANCH-NAME-1234\n" == create_commit_message("some_branch_name_1234")
+    assert "\nSOME-BRANCH-NAME-1234\n" == create_commit_message("some_branch_name_1234_with_description")
+    assert "" == create_commit_message("feature_awesome")
